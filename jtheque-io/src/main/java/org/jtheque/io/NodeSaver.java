b917e9f278cca5d5d56ba0e16d8be172d2af5f7b
wichtounet <baptistewicht@gmail.com>
2010-03-13 21:28:53 +0000

    jtheque-io OSGi Bundle

---
+++ jtheque-io/src/main/java/org/jtheque/io/NodeSaver.java
@@ -0,0 +1,52 @@
+package org.jtheque.io;
+
+/*
+ * This file is part of JTheque.
+ *
+ * JTheque is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * JTheque is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JTheque.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+public class NodeSaver {
+    private NodeSaver() {
+    }
+
+    public static void writeNodes(XMLWriter writer, Iterable<Node> nodes) {
+        for (Node node : nodes) {
+            add(node, writer);
+        }
+    }
+
+    /**
+     * Add the node state to the writer.
+     *
+     * @param node   The node state to add to the writer.
+     * @param writer The XML writer.
+     */
+    private static void add(Node node, XMLWriter writer) {
+        if (node.hasChildren()) {
+            writer.add(node.getName());
+
+            writeNodes(writer, node.getChildrens());
+        } else {
+            writer.add(node.getName(), node.getText());
+        }
+
+        if (node.hasAttribute()) {
+            for (NodeAttribute attribute : node.getAttributes()) {
+                writer.addAttribute(attribute.getKey(), attribute.getValue());
+            }
+        }
+
+        writer.switchToParent();
+    }
+}
---
+++ jtheque-io/src/main/java/org/jtheque/io/XMLUtils.java
@@ -0,0 +1,82 @@
+package org.jtheque.io;
+
+import org.jdom.Document;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
+import org.jtheque.utils.io.FileUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.jtheque.logging.ILoggingManager;
+
+/*
+ * This file is part of JTheque.
+ *
+ * JTheque is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * JTheque is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JTheque.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * An utility class for XML.
+ *
+ * @author Baptiste Wicht
+ */
+public final class XMLUtils {
+    /**
+     * This is an utility class, not instanciable.
+     */
+    private XMLUtils() {
+        super();
+    }
+
+    /**
+     * Save a XML file.
+     *
+     * @param doc  The XML document to save.
+     * @param path The path to the file.
+     */
+    public static void writeXml(Document doc, String path) {
+        XMLOutputter sortie = new XMLOutputter(Format.getPrettyFormat());
+
+        boolean fileOk = true;
+
+        File f = new File(path);
+
+        if (!f.exists()) {
+            try {
+                fileOk = f.createNewFile();
+            } catch (IOException e) {
+                fileOk = false;
+                IOServices.get(ILoggingManager.class).getLogger(XMLUtils.class).error(e);
+            }
+        }
+
+        if (fileOk) {
+            OutputStream stream = null;
+            try {
+                stream = new FileOutputStream(path);
+
+                sortie.output(doc, stream);
+            } catch (FileNotFoundException e) {
+                IOServices.get(ILoggingManager.class).getLogger(XMLUtils.class).error(e);
+            } catch (IOException e) {
+                IOServices.get(ILoggingManager.class).getLogger(XMLUtils.class).error(e);
+            } finally {
+                FileUtils.close(stream);
+            }
+        }
+    }
+}
