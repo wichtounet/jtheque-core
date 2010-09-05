5a42b16951a9c9c06badd5a807f1b7c1e0415a71
wichtounet <baptistewicht@gmail.com>
2010-09-05 11:20:18 +0200

    Improve jtheque-errors tests

--- jtheque-errors/src/test/java/org/jtheque/errors/ErrorServiceTest.java
+++ jtheque-errors/src/test/java/org/jtheque/errors/ErrorServiceTest.java
@@ -8,17 +8,19 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.LoggerFactory;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
 
 import java.io.File;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 /*
  * Copyright JTheque (Baptiste Wicht)
@@ -68,5 +70,71 @@ public class ErrorServiceTest {
     @Test
     public void initOK() {
         assertNotNull(errorService);
+    }
+
+    @Test
+    public void errorsEmptyByDefault() {
+        assertTrue(errorService.getErrors().isEmpty());
+    }
+
+    @Test
+    @DirtiesContext
+    public void addError() {
+        Error error = Errors.newError("Test");
+
+        errorService.addError(error);
+
+        assertEquals(1, errorService.getErrors().size());
+        assertTrue(errorService.getErrors().contains(error));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void unmodifiableCollection() {
+        Error error = Errors.newError("Test");
+
+        errorService.getErrors().remove(error);
+    }
+
+    @Test
+    @DirtiesContext
+    public void listenerCalled() {
+        final Error error = Errors.newError("Test");
+
+        final AtomicInteger integer = new AtomicInteger(0);
+
+        errorService.addErrorListener(new ErrorListener(){
+            @Override
+            public void errorOccurred(Error occuredError) {
+                integer.incrementAndGet();
+
+                assertEquals(error, occuredError);
+            }
+        });
+
+        errorService.addError(error);
+        assertEquals(1, integer.intValue());
+    }
+
+    @Test
+    @DirtiesContext
+    public void listenerRemoved() {
+        final AtomicInteger integer = new AtomicInteger(0);
+
+        ErrorListener listener = new ErrorListener() {
+            @Override
+            public void errorOccurred(Error occuredError) {
+                integer.incrementAndGet();
+            }
+        };
+
+        errorService.addErrorListener(listener);
+
+        errorService.addError(Errors.newError("Test1"));
+
+        errorService.removeErrorListener(listener);
+
+        errorService.addError(Errors.newError("Test2"));
+
+        assertEquals(1, integer.intValue());
     }
 }
---
+++ jtheque-errors/src/test/java/org/jtheque/errors/ErrorsTest.java
@@ -0,0 +1,50 @@
+package org.jtheque.errors;
+
+import org.jtheque.errors.Error.Level;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/*
+ * Copyright JTheque (Baptiste Wicht)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class ErrorsTest {
+    @Test
+    public void title(){
+        Error error = Errors.newError("Title 1");
+
+        assertEquals("Title 1", error.getTitle(null));
+        assertEquals(Level.ERROR, error.getLevel());
+    }
+
+    @Test
+    public void titleLevel() {
+        Error error = Errors.newError("Title 2", Level.WARNING);
+
+        assertEquals("Title 2", error.getTitle(null));
+        assertEquals(Level.WARNING, error.getLevel());
+    }
+
+    @Test
+    public void titleDetails() {
+        Error error = Errors.newError("Title 3", "Details 3");
+
+        assertEquals("Title 3", error.getTitle(null));
+        assertEquals("Details 3", error.getDetails(null));
+        assertEquals(Level.ERROR, error.getLevel());
+    }
+}
