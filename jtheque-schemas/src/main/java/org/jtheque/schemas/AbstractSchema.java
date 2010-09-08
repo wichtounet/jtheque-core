951c94d0861a72a1763cd55fd19bcdf1af3f97e5
wichtounet <baptistewicht@gmail.com>
2010-09-08 23:53:26 +0200

    Add possibility to modify the jdbcTemplate of the context via Spring

--- jtheque-schemas/src/main/java/org/jtheque/schemas/AbstractSchema.java
+++ jtheque-schemas/src/main/java/org/jtheque/schemas/AbstractSchema.java
@@ -43,7 +43,7 @@ public abstract class AbstractSchema implements Schema {
      *
      * @param jdbcTemplate The template to set.
      */
-    void setJdbcTemplate(SimpleJdbcTemplate jdbcTemplate) {
+    public void setJdbcTemplate(SimpleJdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
