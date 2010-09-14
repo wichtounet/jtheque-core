64a9f8f0176f6de8800fd65f95fd426ccdf2584f
wichtounet <baptistewicht@gmail.com>
2010-09-14 12:04:44 +0200

    Separate interface and implementation to avoid CGLib proxy

--- jtheque-core-utils/src/main/java/org/jtheque/core/utils/WebHelper.java
+++ jtheque-core-utils/src/main/java/org/jtheque/core/utils/WebHelper.java
@@ -1,43 +1,10 @@
 package org.jtheque.core.utils;
 
-import org.jtheque.errors.ErrorService;
-import org.jtheque.errors.Errors;
-import org.jtheque.events.EventLevel;
-import org.jtheque.events.EventService;
-import org.jtheque.events.Events;
-import org.jtheque.utils.collections.ArrayUtils;
-import org.jtheque.utils.io.WebUtils;
-
-import javax.annotation.Resource;
-
-/*
- * Copyright JTheque (Baptiste Wicht)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 /**
- * A simple helper to manage services who needs web access.
- *
- * @author Baptiste Wicht
+ * Created by IntelliJ IDEA. User: wichtounet Date: Sep 13, 2010 Time: 2:01:47 PM To change this template use File |
+ * Settings | File Templates.
  */
-public class WebHelper {
-    @Resource
-    private EventService eventService;
-
-    @Resource
-    private ErrorService errorService;
-
+public interface WebHelper {
     /**
      * Indicate if the descriptor is not reachable.
      *
@@ -45,33 +12,5 @@ public class WebHelper {
      *
      * @return true if the descriptor is not reachable else false.
      */
-    public boolean isNotReachable(String url) {
-        if (WebUtils.isURLReachable(url)) {
-            return false;
-        }
-
-        addNotReachableError(url);
-
-        eventService.addEvent(
-                Events.newEvent(EventLevel.ERROR, "System", "events.network.error", EventService.CORE_EVENT_LOG));
-
-        return true;
-    }
-
-    /**
-     * Add a not reachable error to the errors.
-     *
-     * @param url the not reachable URL.
-     */
-    private void addNotReachableError(String url) {
-        if (WebUtils.isInternetReachable()) {
-            errorService.addError(Errors.newI18nError(
-                    "network.resource.title", ArrayUtils.EMPTY_ARRAY,
-                    "network.resource", new Object[]{url}));
-        } else {
-            errorService.addError(Errors.newI18nError(
-                    "network.internet.title", ArrayUtils.EMPTY_ARRAY,
-                    "network.internet", new Object[]{url}));
-        }
-    }
+    boolean isNotReachable(String url);
 }
---
+++ jtheque-core-utils/src/main/java/org/jtheque/core/utils/impl/SimpleWebHelper.java
@@ -0,0 +1,72 @@
+package org.jtheque.core.utils.impl;
+
+import org.jtheque.core.utils.WebHelper;
+import org.jtheque.errors.ErrorService;
+import org.jtheque.errors.Errors;
+import org.jtheque.events.EventLevel;
+import org.jtheque.events.EventService;
+import org.jtheque.events.Events;
+import org.jtheque.utils.collections.ArrayUtils;
+import org.jtheque.utils.io.WebUtils;
+
+import javax.annotation.Resource;
+
+/*
+ * Copyright JTheque (Baptiste Wicht)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * A simple helper to manage services who needs web access.
+ *
+ * @author Baptiste Wicht
+ */
+public class SimpleWebHelper implements WebHelper {
+    @Resource
+    private EventService eventService;
+
+    @Resource
+    private ErrorService errorService;
+
+    @Override
+    public boolean isNotReachable(String url) {
+        if (WebUtils.isURLReachable(url)) {
+            return false;
+        }
+
+        addNotReachableError(url);
+
+        eventService.addEvent(
+                Events.newEvent(EventLevel.ERROR, "System", "events.network.error", EventService.CORE_EVENT_LOG));
+
+        return true;
+    }
+
+    /**
+     * Add a not reachable error to the errors.
+     *
+     * @param url the not reachable URL.
+     */
+    private void addNotReachableError(String url) {
+        if (WebUtils.isInternetReachable()) {
+            errorService.addError(Errors.newI18nError(
+                    "network.resource.title", ArrayUtils.EMPTY_ARRAY,
+                    "network.resource", new Object[]{url}));
+        } else {
+            errorService.addError(Errors.newI18nError(
+                    "network.internet.title", ArrayUtils.EMPTY_ARRAY,
+                    "network.internet", new Object[]{url}));
+        }
+    }
+}
